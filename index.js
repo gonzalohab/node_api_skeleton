a2ff99960a79708b23e5dde849825d3ec05a932f
Gonzalo Andrade <gonzalohab@gmail.com>
2022-06-27 02:33:19 -0400

    Remove comment index.js

--- index.js
+++ index.js
@@ -9,38 +9,7 @@ const database = require('./config/db');
 
 (async () => {
     await database.createTables();
-})()
-
-/*
-const mysql = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: process.env.DB_HOST || '127.0.0.1',
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USER || 'your_database_user',
-        password: process.env.DB_PASSWORD || 'your_database_password',
-        database: process.env.DB_NAME || 'myapp_test'
-    }
-});
-*/
-/*
-(async () => {
-    const tableName = 'users';
-    try {
-        await mysql.schema.createTable(tableName, function (table) {
-            table.increments();
-            table.string('name');
-            table.string('email');
-            table.string('password');
-            table.string('picture');
-            table.timestamps();
-        });
-        console.log(`${tableName} created ok!`);
-    } catch (err) {
-        console.log(`${tableName} table already exists`);
-    }
 })();
-*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
